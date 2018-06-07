fb8d9fe3547b7dfce9ec9098771bdde501de3129
srishti77 <=>
2018-06-07 16:56:55 +0200

    First Commit

---
+++ sample.py
@@ -0,0 +1,15 @@
+import logging
+from pandas.tests.io.json.test_pandas import cat
+
+from fastai.Recommend import RecommendDataset
+from fastai.column_data import ColumnarModelData
+from fastai.learner import *
+
+import torchtext
+from torchtext import vocab, data
+from torchtext.datasets import language_modeling
+from fastai.rnn_reg import *
+from fastai.rnn_train import *
+from fastai.nlp import *
+from fastai.lm_rnn import *
+import dill as pickle
