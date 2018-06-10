93ea4b7eb5f1fdcdadce3da04644ada4ff4641aa
srishti77 <=>
2018-06-10 17:24:00 +0200

    change the active data directory

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 import data
-dir_PATH='D:\\Thesis\\fastai\\fastai\\data\\recommend_50000'
+dir_PATH='D:\\Thesis\\fastai\\fastai\\data\\recommend_10000'
 PATH='D:\\Thesis\\fastai\\fastai\\data'
 # bs=32
 bs=8
@@ -9,4 +9,4 @@ nh = 100     # number of hidden activations per layer
 nl = 3
 
 
-pretrained_lang_model_name='recommend_50000'
+pretrained_lang_model_name='recommend_10000'
